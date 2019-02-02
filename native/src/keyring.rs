1e35136754f33ef168bcb565db94bfd096fa67fd
Alexander McCormick <alexandermccormick@protonmail.com>
2019-02-01 17:45:58 -0800

    moved base logic around, improved error handling

--- native/src/keyring.rs
+++ native/src/keyring.rs
@@ -14,48 +14,44 @@ pub struct Keyring {
 impl Keyring {
   pub fn new(keyring_path_buf: &str) -> Keyring {
     let mut keyring_path_buf = PathBuf::from(keyring_path_buf);
-    match &keyring_path_buf.exists() {
-      true => Keyring::read_keys(&mut keyring_path_buf),
-      false => {
-        let (public_key, secret_key) = Keyring::create_keys(&mut keyring_path_buf);
-
-        Keyring {
-          public_key,
-          secret_key
-        }
+    match (keyring_path_buf.exists(), keyring_path_buf.is_dir()) {
+      (true, true) => Keyring::read_keys(&mut keyring_path_buf),
+      (false, false) => {
+        create_dir_all(&keyring_path_buf).ok().expect("Could not create path for keys!!!");
+        Keyring::create_keys(&mut keyring_path_buf)
       },
+      (true, false) => panic!("Given PATH does not lead to directory!!!"),
+      (false, true) => panic!("This shouldn't be possible! Given PATH does not exist, but is directory!!!")
     }
   }
 
-  fn create_keys(keyring_path_buf: &mut PathBuf) -> (PublicKey, SecretKey) {
+  fn create_keys(keyring_path_buf: &mut PathBuf) -> Keyring {
     let (public_key, secret_key) = sign::gen_keypair();
 
-    match keyring_path_buf.is_dir() {
-      true => (),
-      false => create_dir_all(&keyring_path_buf).ok().expect("Could not create path!!!"),
-    };
+    Keyring::write_public_key(&public_key.0, keyring_path_buf);
+    Keyring::write_secret_key(&secret_key.0[..], keyring_path_buf);
 
-    Keyring::write_keys(&public_key.0, &secret_key.0[..], keyring_path_buf);
-
-    (public_key, secret_key)
+    Keyring {
+      public_key,
+      secret_key
+    }
   }
 
-  fn write_keys(pk: &[u8; 32], sk: &[u8], keyring_path_buf: &mut PathBuf) {
-    match keyring_path_buf.is_dir() {
-      false => panic!("Given path does not point to directory!!!"),
-      true => {
-        keyring_path_buf.push("public.key");
-        write_file(&keyring_path_buf, pk).ok();
+  fn write_public_key(public_key: &[u8; 32], keyring_path_buf: &mut PathBuf) {
+    keyring_path_buf.push("public.key");
+    write_file(&keyring_path_buf, public_key).ok();
+    keyring_path_buf.pop();
+  }
 
-        keyring_path_buf.set_file_name("secret.key");
-        write_file(&keyring_path_buf, sk).ok();
-      }
-    }
+  fn write_secret_key(secret_key: &[u8], keyring_path_buf: &mut PathBuf) {
+    keyring_path_buf.push("secret.key");
+    write_file(&keyring_path_buf, secret_key).ok();
+    keyring_path_buf.pop();
   }
 
   // TODO: check for keys in dir first
   fn read_keys(keyring_path_buf: &mut PathBuf) -> Keyring {
-    match keyring_path_buf.is_dir() {
+    match Keyring::contains_keys(keyring_path_buf) {
       true => {
         let public_key = Keyring::load_pub_key(keyring_path_buf);
         let secret_key = Keyring::load_secret_key(keyring_path_buf);
@@ -65,41 +61,49 @@ impl Keyring {
           secret_key,
         }
       },
-      false => panic!("Given PATH does not lead to directory")
+      false => Keyring::create_keys(keyring_path_buf)
+    }
+  }
+  fn contains_keys(keyring_path_buf: &mut PathBuf) -> bool {
+    keyring_path_buf.push("public.key");
+    let has_public = match (keyring_path_buf.exists(), keyring_path_buf.is_file()) {
+      (true, true) => true,
+      (false, false) => false,
+      (true, false) => panic!("Given PUBLIC_KEY_PATH does not lead to file!!!"),
+      (false, true) => panic!("This shouldn't be possible! Given PATH does not exist, but is file!!!")
+    };
+    keyring_path_buf.set_file_name("secret.key");
+    let has_secret = match (keyring_path_buf.exists(), keyring_path_buf.is_file()) {
+      (true, true) => true,
+      (false, false) => false,
+      (true, false) => panic!("Given SECRET_KEY_PATH does not lead to file!!!"),
+      (false, true) => panic!("This shouldn't be possible! Given PATH does not exist, but is file!!!")
+    };
+    keyring_path_buf.pop();
+
+    match (&has_public, &has_secret) {
+      (true, true) => true,
+      (false, true) => panic!("Missing public key!!!"),
+      (true, false) => panic!("Missing secret key!!!"),
+      (false, false) => false
     }
   }
 
   fn load_pub_key(keyring_path_buf: &mut PathBuf) -> PublicKey {
     keyring_path_buf.push("public.key");
-    match keyring_path_buf.is_file() {
-      true => {
-        let mut pub_key_file = File::open(&keyring_path_buf).unwrap();
-        let mut pub_key_arr: [u8; 32] = [0; 32];
-        pub_key_file.read_exact(&mut pub_key_arr).ok();
-        PublicKey(pub_key_arr)
-      },
-      false => {
-        keyring_path_buf.pop();
-        let (public_key, _secret_key) = Keyring::create_keys(keyring_path_buf);
-        public_key
-      }
-    }
+    let mut pub_key_file = File::open(&keyring_path_buf).unwrap();
+    let mut pub_key_arr: [u8; 32] = [0; 32];
+    pub_key_file.read_exact(&mut pub_key_arr).ok();
+    keyring_path_buf.pop();
+    PublicKey(pub_key_arr)
   }
 
   fn load_secret_key(keyring_path_buf: &mut PathBuf) -> SecretKey {
     keyring_path_buf.set_file_name("secret.key");
-    match keyring_path_buf.is_file() {
-      true => {
-      let mut secret_key_file = File::open(&keyring_path_buf).unwrap();
-      let mut secret_key_arr: [u8; 64] = [0; 64];
-      secret_key_file.read_exact(&mut secret_key_arr).ok();
-      SecretKey(secret_key_arr)
-      },
-      false => {
-        keyring_path_buf.pop();
-        let (_public_key, secret_key) = Keyring::create_keys(keyring_path_buf);
-        secret_key
-      }
-    }
+    let mut secret_key_file = File::open(&keyring_path_buf).unwrap();
+    let mut secret_key_arr: [u8; 64] = [0; 64];
+    secret_key_file.read_exact(&mut secret_key_arr).ok();
+    keyring_path_buf.pop();
+    SecretKey(secret_key_arr)
   }
 }
